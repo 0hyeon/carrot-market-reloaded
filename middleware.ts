250c51cbe7405402a55e1be0155fbc37fa03b627
0hyeon <djdjdjk2006@naver.com>
2024-03-26 23:31:32 +0900

    feat: auth middleware+

---
+++ middleware.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+export async function middleware(req: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[req.nextUrl.pathname];
+  if (!session.id) {
+    //로그인후 세션이없고
+    if (!exists) {
+      //publicOnlyUrls가 허락하는 페이지 아니면,
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else {
+    //로그인후
+    if (exists) {
+      //publicOnlyUrls 외에 다른페이지 가려하면
+      return NextResponse.redirect(new URL("/products", req.url));
+    }
+  }
+  //   const pathname = req.nextUrl.pathname;
+  //   if (pathname === "/") {
+  //     const res = NextResponse.next();
+  //     res.cookies.set("middleware-cookie", "hello");
+  //     return res;
+  //   }
+  //   if (pathname === "/profile") {
+  //     return NextResponse.redirect(new URL("/", req.url));
+  //   }
+}
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    // {
+    //   source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    //   missing: [
+    //     { type: "header", key: "next-router-prefetch" },
+    //     { type: "header", key: "purpose", value: "prefetch" },
+    //   ],
+    // },
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
