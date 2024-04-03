3a02259080eea5b8236461c0737a580af0ecb35e
0hyeon <djdjdjk2006@naver.com>
2024-04-03 23:51:54 +0900

    feat: Product Detail

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,11 @@
-export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+async function getProduct() {
+  await new Promise((resolve) => setTimeout(resolve, 60000));
+}
+export default async function ProductDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await getProduct();
+  return <div>ProductDetail detail of the product{id}</div>;
 }
