6095f2932c31e14cb4ca16b0134a765d145a3670
0hyeon <djdjdjk2006@naver.com>
2024-04-08 22:02:48 +0900

    fix: productDetial

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,38 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 60000));
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
 }
+async function getProduct(id: number) {
+  const product = db.product.findUnique({
+    where: {
+      id,
+    },
+  });
+  return product;
+}
+
 export default async function ProductDetail({
-  params: { id },
+  params,
 }: {
   params: { id: string };
 }) {
-  const product = await getProduct();
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const product = await getProduct(id);
+
+  if (!product) {
+    return notFound();
+  }
+  const isOwner = await getIsOwner(product.userId);
+
   return <div>ProductDetail detail of the product{id}</div>;
 }
