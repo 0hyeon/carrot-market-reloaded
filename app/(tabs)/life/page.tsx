c4175c55c6221393bf7a225d3bb6e5767b8f25a1
0hyeon <djdjdjk2006@naver.com>
2024-06-09 22:10:05 +0900

    feat: life.page

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,3 +1,12 @@
-export default function Life() {
+import db from "@/lib/db";
+
+async function getPosts() {
+  // await new Promise((r) => setTimeout(r, 100000));
+  const posts = await db.post.findMany({});
+  return posts;
+}
+
+export default async function Life() {
+  const posts = await getPosts();
   return <div className="text-white text-4xl">Life!</div>;
 }
